e6b4a34cabaf81b479b065b1f8372a29afbb4aa2
Nishant <bhagatnishant5454@gmail.com>
2024-05-04 18:58:51 +0530

    added appointments

--- Controller/usercontroll.js
+++ Controller/usercontroll.js
@@ -1,7 +1,8 @@
 import Usermodel from '../models/Usermodel.js'; 
 import Doctormodel from '../models/Doctormodel.js'; 
 import Bookingmodel from '../models/Bookingmodel.js'; 
-import bcrypt from "bcrypt"; 
+import bcrypt from "bcrypt";  
+import moment from "moment";
 export const register=async(req,res,next)=>{ 
     try{ // User Registration
         const {name,email,password}=req.body; 
@@ -85,11 +86,157 @@ export const applydoctor=async(req,res)=>{
         const Doctor=new Doctormodel(req.body); 
         await Doctor.save(); 
         const getAdmin=await Usermodel.findOne({isAdmin:true}); 
-        const unSeennotification=getAdmin.unSeennotification
+        const unSeennotification=getAdmin.unSeennotification 
+        unSeennotification.push({ 
+            type:"New Application come for the Doctor Request",
+            message:`${Doctor.first_name} ${Doctor.last_name} has applied for the Role of Docotr`,
+            data:{ 
+                doctorId:Doctor._Id, 
+                name:Doctor.first_name+Doctor.last_name, 
+
+            },
+            onClickPath:"admin/doctors",
+        }); 
+        await Usermodel.findByIdAndUpdate(getAdmin._id,{unSeennotification}); 
+        // Response from the Server 
+        return res.status(201).json({msg:'Application Sucsessfully Submiited',});
     } 
-    
+
     catch(err){
         res.status(404).json({msg:'Internal Server Error in the System'})
     }
-}
+};  
+export const markAllNotificationsAsSeen = async (req, res, next) => {
+    try {
+      const user = await UserModel.findOne({ _id: req.body.userId });
+      const unSeenNotifications = user.unSeenNotifications;
+  
+      //Append unSeenNotifications to seenNotifications array
+      user.seenNotifications.push(...unSeenNotifications);
+  
+      //Clear unSeenNotifications array
+      user.unSeenNotifications = [];
+  
+      //Save the Updated user
+      const updatedUser = await user.save();
+      updatedUser.password = undefined; //Password hide
+      return res.status(200).json({
+        success: true,
+        message: "All notifications marked as seen!",
+        data: updatedUser,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        message: "Internal Server Error!",
+        success: false,
+        error: err.message,
+      });
+    }
+  };
+  
+  //************ DELETE ALL SEEN NOTIFICATIONS *******************/
+  export const deleteAllSeenNotifications = async (req, res, next) => {
+    try {
+      const user = await UserModel.findOne({ _id: req.body.userId });
+  
+      //Clear the seenNotifications array
+      user.seenNotifications = [];
+  
+      //Save the updated user
+      const updatedUser = await user.save();
+      updatedUser.password = undefined;
+  
+      return res.status(200).json({
+        success: true,
+        message: "All seen notifications deleted!",
+        data: updatedUser,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Internal Server Error!",
+        error: err.message,
+      });
+    }
+  };
+  
+  //************** GET ALL APPROVED DOCTORS ***********/
+  export const getAllApprovedDoctors = async (req, res) => {
+    try {
+      const doctors = await DoctorModel.find({ status: "approved" });
+      if (!doctors) {
+        return res.status(404).json({
+          success: false,
+          message: "Doctor not found.",
+        });
+      }
+  
+      //success res
+      return res.status(200).json({
+        success: true,
+        message: "Doctor list fetched successfully!",
+        data: doctors,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Internal Server Error!",
+        error: err.message,
+      });
+    }
+  };
+  
+  //************** BOOK APPOINTMENTS ***********/
+  export const bookingAppointment = async (req, res) => {
+    try {
+      //Get data
+      let { doctorId, userId, doctorInfo, userInfo, date, time, status } =
+        req.body;
+      if (!date || !time) {
+        return res.status(422).json({
+          success: false,
+          message: "Please select date and time!",
+        });
+      }
+  
+      //Change date and time to ISO String (Used for convert date object to string obj) for check availability
+      date = moment(date, "DD-MM-YYYY").toISOString();
+      time = moment(time, "HH:mm").toISOString();
+      status = "pending";
+  
+      //Add bookings
+      const newBookings = new BookingModel({
+        doctorId,
+        userId,
+        doctorInfo,
+        userInfo,
+        date,
+        time,
+        status,
+      });
+      await newBookings.save();
+  
+      //Push notification to user
+      const user = await UserModel.findOne({ _id: req.body.doctorInfo.userId });
+      user.unSeenNotifications.push({
+        type: "New-Booking-Request",
+        message: `A new appointment booking request from ${req.body.userInfo.name}`,
+        onClickPath: "/user/appointments",
+      });
+      await user.save();
+  
+      return res.status(200).json({
+        success: true,
+        message: "Appointment booking successfully!",
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Internal Server Error!",
+        error: err.message,
+      });
+    }
+  };
+  
+
 
